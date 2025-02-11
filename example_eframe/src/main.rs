076b69c19e82872214ef4fb77bdfd0567b91c1ab
evanyuan <evanyuan163@163.com>
2025-02-11 14:27:01 +0800

    i18n, sys-locale test

--- example_eframe/src/main.rs
+++ example_eframe/src/main.rs
@@ -1,6 +1,8 @@
 fn main() -> eframe::Result<()> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
+    let locale = sys_locale::get_locale().or_else(||Some("en-US".to_string())).unwrap();
+    println!("locale: {}",&locale);
     eframe::run_native(
         "ehttp demo",
         Default::default(),
